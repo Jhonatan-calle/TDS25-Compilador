78d781fdfda87afac1cda1908fdde68267b89610
Joaquin Tissera <phoenix_css@hotmail.com>
2025-09-10 11:24:07 -0300

    Change name of the compilator

--- compile.sh
+++ compile.sh
@@ -3,5 +3,5 @@
 echo "Starting compilation Flex and Bison"
 bison -d -Wall syntax.y
 flex lexicon.l
-gcc -Wall -o parser syntax.tab.c lex.yy.c -lfl
+gcc -Wall -o c-tds syntax.tab.c lex.yy.c -lfl
 echo "End compilation!"
