5e9eb00f9d7fe032fedfb1d89050aecc8924136d
Jhonatan-calle <jhonatancallegaleano@gmail.com>
2025-09-19 01:46:05 -0300

    relocation simbols.c

--- src/simbolos.c
+++ src/simbolos.c
@@ -1,12 +1,11 @@
+#include "../headers/simbolos.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../headers/simbolos.h"
 
 TablaSimbolos *t = NULL;
 
-TablaSimbolos *crear_tabla(size_t capacidad_inicial)
-{
+TablaSimbolos *crear_tabla(size_t capacidad_inicial) {
   TablaSimbolos *t = malloc(sizeof(TablaSimbolos));
   t->capacidad = capacidad_inicial;
   t->usados = 0;
@@ -14,8 +13,7 @@ TablaSimbolos *crear_tabla(size_t capacidad_inicial)
   return t;
 }
 
-void insertar_simbolo(Simbolo *e)
-{
+void insertar_simbolo(Simbolo *e) {
   // buscar si ya existe
   // for (size_t i = 0; i < t->usados; i++) {
   //     if (strcmp(t->tabla[i].nombre, nombre) == 0) {
@@ -24,8 +22,7 @@ void insertar_simbolo(Simbolo *e)
   //         return;
   //     }
   // }
-  if (t->usados >= t->capacidad)
-  {
+  if (t->usados >= t->capacidad) {
     t->capacidad *= 2;
     t->tabla = realloc(t->tabla, t->capacidad * sizeof(Simbolo *));
   }
@@ -33,22 +30,17 @@ void insertar_simbolo(Simbolo *e)
   t->usados++;
 }
 
-Simbolo *buscar_simbolo(char *nombre)
-{
-  for (size_t i = 0; i < t->usados; i++)
-  {
-    if (strcmp(t->tabla[i]->nombre, nombre) == 0)
-    {
+Simbolo *buscar_simbolo(char *nombre) {
+  for (size_t i = 0; i < t->usados; i++) {
+    if (strcmp(t->tabla[i]->nombre, nombre) == 0) {
       return t->tabla[i];
     }
   }
   return NULL; // no encontrado
 }
 
-void liberar_tabla()
-{
-  for (size_t i = 0; i < t->usados; i++)
-  {
+void liberar_tabla() {
+  for (size_t i = 0; i < t->usados; i++) {
     free(t->tabla[i]);
   }
   free(t->tabla);
