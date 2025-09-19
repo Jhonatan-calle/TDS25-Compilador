745422e1ecd3b0d0764c3e2737f79cfbda267cf4
Jhonatan-calle <jhonatancallegaleano@gmail.com>
2025-09-19 01:43:02 -0300

    solution linking problems

--- compile.sh
+++ compile.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 echo "Starting compilation Flex and Bison"
-gcc -Wall -c src/main.c -o build/main.o
-gcc -Wall -c src/utils.c -o build/utils.o
 bison -d -Wall syntax.y
 flex lexicon.l
-gcc -Wall -Wno-unused-function syntax.tab.c lex.yy.c build/main.o build/utils.o -o c-tds -lfl
+
+echo "Compiling sources"
+gcc -Wall -c src/main.c -o build/main.o
+gcc -Wall -c src/utils.c -o build/utils.o
+gcc -Wall -c src/ast.c -o build/ast.o
+gcc -Wall -c src/simbolos.c -o build/simbolos.o
+gcc -Wall -c src/compiler.c -o build/compiler.o
+
+echo "Linking"
+gcc -Wall -Wno-unused-function syntax.tab.c lex.yy.c \
+  build/main.o build/utils.o build/ast.o build/simbolos.o build/compiler.o \
+  -o c-tds -lfl
+
 echo "End compilation!"
