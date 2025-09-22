2a0c10612f1d277daf842724899f1669c15589ed
Jhonatan-calle <jhonatancallegaleano@gmail.com>
2025-09-22 12:00:09 -0300

    fix syntax error

--- src/utils.c
+++ src/utils.c
@@ -1,39 +1,33 @@
-#include <stdio.h>
 #include "../headers/utils.h"
 #include "../headers/simbolos.h"
+#include <stdio.h>
 
 int temp_counter = 0;
 
-char *new_temp()
-{
+char *new_temp() {
   char buffer[16];
   snprintf(buffer, sizeof(buffer), "t%d", temp_counter++);
   return strdup(buffer);
 }
 
-char *gen_code(AST *node)
-{
+char *gen_code(AST *node) {
   if (!node)
     return NULL;
 
-  switch (node->type)
-  {
-  case TR_VALOR:
-  {
+  switch (node->type) {
+  case TR_VALOR: {
     char *t = new_temp();
-    printf("LOAD %s, %d\n", t, simbol->valor);
+    printf("LOAD %s, %d\n", t, node->info->valor);
     return t;
   }
 
-  case TR_IDENTIFICADOR:
-  {
+  case TR_IDENTIFICADOR: {
     char *t = new_temp();
-    printf("LOAD %s, %s\n", t, simbol->nombre);
+    printf("LOAD %s, %s\n", t, node->info->nombre);
     return t;
   }
 
-  case TR_ASIGNACION:
-  {
+  case TR_ASIGNACION: {
     // child[0] = identificador
     // child[1] = expresión
     char *rhs = gen_code(node->childs[1]);
@@ -41,8 +35,7 @@ char *gen_code(AST *node)
     return rhs;
   }
 
-  case TR_SUMA:
-  {
+  case TR_SUMA: {
     char *lhs = gen_code(node->childs[0]);
     char *rhs = gen_code(node->childs[1]);
     char *t = new_temp();
@@ -50,8 +43,7 @@ char *gen_code(AST *node)
     return t;
   }
 
-  case TR_MULTIPLICACION:
-  {
+  case TR_MULTIPLICACION: {
     char *lhs = gen_code(node->childs[0]);
     char *rhs = gen_code(node->childs[1]);
     char *t = new_temp();
@@ -59,8 +51,7 @@ char *gen_code(AST *node)
     return t;
   }
 
-  case TR_AND:
-  {
+  case TR_AND: {
     char *lhs = gen_code(node->childs[0]);
     char *rhs = gen_code(node->childs[1]);
     char *t = new_temp();
@@ -68,8 +59,7 @@ char *gen_code(AST *node)
     return t;
   }
 
-  case TR_OR:
-  {
+  case TR_OR: {
     char *lhs = gen_code(node->childs[0]);
     char *rhs = gen_code(node->childs[1]);
     char *t = new_temp();
@@ -77,15 +67,13 @@ char *gen_code(AST *node)
     return t;
   }
 
-  case TR_LISTA_SENTENCIAS:
-  {
+  case TR_LISTA_SENTENCIAS: {
     for (int i = 0; i < node->child_count; i++)
       gen_code(node->childs[i]);
     return NULL;
   }
 
-  case TR_PROGRAMA:
-  {
+  case TR_PROGRAMA: {
     printf("; inicio programa\n");
     gen_code(node->childs[0]);
     printf("; fin programa\n");
