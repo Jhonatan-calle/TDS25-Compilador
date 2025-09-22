34fab305d61840cf15562899cfd3433a719037f9
Jhonatan-calle <jhonatancallegaleano@gmail.com>
2025-09-22 11:56:27 -0300

    stop showing shift/reduce examples warnings

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Starting compilation Flex and Bison"
-bison -d -Wall -Wcounterexamples syntax.y
+bison -d -Wall syntax.y
 flex lexicon.l
 
 echo "Compiling sources"
@@ -13,5 +13,5 @@ gcc -Wall -c src/simbolos.c -o build/simbolos.o
 echo "Linking"
 gcc -Wall -Wno-unused-function syntax.tab.c lex.yy.c \
   build/main.o build/utils.o build/ast.o build/simbolos.o \
-  -o c-tds -lfl 
+  -o c-tds -lfl
 echo "End compilation!"
