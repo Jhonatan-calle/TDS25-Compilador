9e40bafbad9dedd7832b0e7901599402de4ea54b
Joaquin Tissera <phoenix_css@hotmail.com>
2025-11-03 10:30:42 -0300

    Refactor assembly switch case modules

---
+++ headers/assembly_modules.h
@@ -0,0 +1,30 @@
+#include "ast.h"
+#include "symbols.h"
+#include "three_address_code.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void assembly_module_add(FILE *asm_out, TAC *t);
+void assembly_module_sub(FILE *asm_out, TAC *t);
+void assembly_module_mul(FILE *asm_out, TAC *t);
+void assembly_module_div(FILE *asm_out, TAC *t);
+void assembly_module_mod(FILE *asm_out, TAC *t);
+void assembly_module_less(FILE *asm_out, TAC *t);
+void assembly_module_gr(FILE *asm_out, TAC *t);
+void assembly_module_eq(FILE *asm_out, TAC *t);
+void assembly_module_and(FILE *asm_out, TAC *t);
+void assembly_module_or(FILE *asm_out, TAC *t);
+void assembly_module_not(FILE *asm_out, TAC *t);
+void assembly_module_neg(FILE *asm_out, TAC *t);
+void assembly_module_assign(FILE *asm_out, TAC *t);
+void assembly_module_label(FILE *asm_out, TAC *t);
+void assembly_module_label_if(FILE *asm_out, TAC *t);
+void assembly_module_goto(FILE *asm_out, TAC *t);
+void assembly_module_ifz(FILE *asm_out, TAC *t);
+void assembly_module_param(FILE *asm_out, TAC *t);
+void assembly_module_arg(FILE *asm_out, TAC *t);
+void assembly_module_call(FILE *asm_out, TAC *t);
+void assembly_module_return(FILE *asm_out, TAC *t);
+void assembly_module_extern(FILE *asm_out, TAC *t);
--- src/assembly_code.c
+++ src/assembly_code.c
@@ -221,339 +221,99 @@ void gen_assembly_code(TAC *head) {
     switch (t->op) {
 
     // --- Arithmetic operations ---
-    case TAC_ADD: {
-      reset_arg_registers();
-      // We use %r10 as a temporal register (scratch)
-      // We support immediate operands (offset == 0) and memory
-      if (t->op1->offset == 0)
-        fprintf(asm_out, "  mov $%d, %%r10\n", t->op1->value);
-      else
-        fprintf(asm_out, "  mov %d(%%rbp), %%r10\n", t->op1->offset);
-
-      if (t->op2->offset == 0)
-        fprintf(asm_out, "  add $%d, %%r10\n", t->op2->value);
-      else
-        fprintf(asm_out, "  add %d(%%rbp), %%r10\n", t->op2->offset);
-
-      fprintf(asm_out, "  mov %%r10, %d(%%rbp)\n", t->result->offset);
+    case TAC_ADD:
+      assembly_module_add(asm_out, t);
       break;
-    }
 
-    case TAC_SUB: {
-      reset_arg_registers();
-      // result = op1 - op2
-      if (t->op1->offset == 0)
-        fprintf(asm_out, "  mov $%d, %%r10\n", t->op1->value);
-      else
-        fprintf(asm_out, "  mov %d(%%rbp), %%r10\n", t->op1->offset);
-
-      if (t->op2->offset == 0)
-        fprintf(asm_out, "  sub $%d, %%r10\n", t->op2->value);
-      else
-        fprintf(asm_out, "  sub %d(%%rbp), %%r10\n", t->op2->offset);
-
-      fprintf(asm_out, "  mov %%r10, %d(%%rbp)\n", t->result->offset);
+    case TAC_SUB:
+      assembly_module_sub(asm_out, t);
       break;
-    }
 
-    case TAC_MUL: {
-      reset_arg_registers();
-      // imul supports reg, r/m form: imul <r/m64>, <reg>
-      if (t->op1->offset == 0)
-        fprintf(asm_out, "  mov $%d, %%r10\n", t->op1->value);
-      else
-        fprintf(asm_out, "  mov %d(%%rbp), %%r10\n", t->op1->offset);
-
-      if (t->op2->offset == 0)
-        fprintf(asm_out, "  imul $%d, %%r10\n",
-                t->op2->value); // immediate multiply
-      else
-        fprintf(asm_out, "  imul %d(%%rbp), %%r10\n", t->op2->offset);
-
-      fprintf(asm_out, "  mov %%r10, %d(%%rbp)\n", t->result->offset);
+    case TAC_MUL:
+      assembly_module_mul(asm_out, t);
       break;
-    }
 
-    case TAC_DIV: {
-      reset_arg_registers();
-      // idiv requires dividend in rax, sign-extend in rdx via cqo
-      if (t->op1->offset == 0)
-        fprintf(asm_out, "  mov $%d, %%rax\n", t->op1->value);
-      else
-        fprintf(asm_out, "  mov %d(%%rbp), %%rax\n", t->op1->offset);
-
-      fprintf(asm_out, "  cqo\n");
-
-      if (t->op2->offset == 0)
-        fprintf(asm_out, "  mov $%d, %%r10\n  idiv %%r10\n", t->op2->value);
-      else
-        fprintf(asm_out, "  idiv %d(%%rbp)\n", t->op2->offset);
-
-      fprintf(asm_out, "  mov %%rax, %d(%%rbp)\n", t->result->offset);
+    case TAC_DIV:
+      assembly_module_div(asm_out, t);
       break;
-    }
 
-    case TAC_MOD: {
-      reset_arg_registers();
-      // remainder stored in rdx after idiv
-      if (t->op1->offset == 0)
-        fprintf(asm_out, "  mov $%d, %%rax\n", t->op1->value);
-      else
-        fprintf(asm_out, "  mov %d(%%rbp), %%rax\n", t->op1->offset);
-
-      fprintf(asm_out, "  cqo\n");
-
-      if (t->op2->offset == 0)
-        fprintf(asm_out, "  mov $%d, %%r10\n  idiv %%r10\n", t->op2->value);
-      else
-        fprintf(asm_out, "  idiv %d(%%rbp)\n", t->op2->offset);
-
-      fprintf(asm_out, "  mov %%rdx, %d(%%rbp)\n", t->result->offset);
+    case TAC_MOD:
+      assembly_module_mod(asm_out, t);
       break;
-    }
 
     // --- Comparations ---
-    case TAC_LESS: {
-      reset_arg_registers();
-      // result = op1 < op2
-      if (t->op1->offset == 0)
-        fprintf(asm_out, "  mov $%d, %%r10\n", t->op1->value);
-      else
-        fprintf(asm_out, "  mov %d(%%rbp), %%r10\n", t->op1->offset);
-
-      if (t->op2->offset == 0)
-        fprintf(asm_out, "  cmp $%d, %%r10\n", t->op2->value);
-      else
-        fprintf(asm_out, "  cmp %d(%%rbp), %%r10\n", t->op2->offset);
-
-      fprintf(asm_out, "  setl %%al\n");
-      fprintf(asm_out, "  movzbq %%al, %%r10\n");
-      fprintf(asm_out, "  mov %%r10, %d(%%rbp)\n", t->result->offset);
+    case TAC_LESS:
+      assembly_module_less(asm_out, t);
       break;
-    }
 
-    case TAC_GR: {
-      reset_arg_registers();
-      // result = op1 > op2
-      if (t->op1->offset == 0)
-        fprintf(asm_out, "  mov $%d, %%r10\n", t->op1->value);
-      else
-        fprintf(asm_out, "  mov %d(%%rbp), %%r10\n", t->op1->offset);
-
-      if (t->op2->offset == 0)
-        fprintf(asm_out, "  cmp $%d, %%r10\n", t->op2->value);
-      else
-        fprintf(asm_out, "  cmp %d(%%rbp), %%r10\n", t->op2->offset);
-
-      fprintf(asm_out, "  setg %%al\n");
-      fprintf(asm_out, "  movzbq %%al, %%r10\n");
-      fprintf(asm_out, "  mov %%r10, %d(%%rbp)\n", t->result->offset);
+    case TAC_GR:
+      assembly_module_gr(asm_out, t);
       break;
-    }
 
-    case TAC_EQ: {
-      reset_arg_registers();
-      // result = op1 == op2
-      if (t->op1->offset == 0)
-        fprintf(asm_out, "  mov $%d, %%r10\n", t->op1->value);
-      else
-        fprintf(asm_out, "  mov %d(%%rbp), %%r10\n", t->op1->offset);
-
-      if (t->op2->offset == 0)
-        fprintf(asm_out, "  mov $%d, %%r11\n", t->op2->value);
-      else
-        fprintf(asm_out, "  mov %d(%%rbp), %%r11\n", t->op2->offset);
-
-      fprintf(asm_out, "  cmp %%r10, %%r11\n");
-      fprintf(asm_out, "  mov $0, %%r11\n");
-      fprintf(asm_out, "  mov $1, %%r10\n");
-      fprintf(asm_out, "  cmove %%r10, %%r11\n");
-      fprintf(asm_out, "  mov %%r11, %d(%%rbp)\n", t->result->offset);
+    case TAC_EQ:
+      assembly_module_eq(asm_out, t);
       break;
-    }
 
     // --- Logicals ---
-    case TAC_AND: {
-      reset_arg_registers();
-      // booleanize op1 and op2, then and
-      // use r10 and r11 as scratch
-      if (t->op1->offset == 0)
-        fprintf(asm_out, "  mov $%d, %%r10\n", t->op1->value);
-      else
-        fprintf(asm_out, "  mov %d(%%rbp), %%r10\n", t->op1->offset);
-
-      fprintf(asm_out, "  cmp $0, %%r10\n");
-      fprintf(asm_out, "  setne %%al\n");
-      fprintf(asm_out, "  movzbq %%al, %%r10\n");
-
-      if (t->op2->offset == 0)
-        fprintf(asm_out, "  mov $%d, %%r11\n", t->op2->value);
-      else
-        fprintf(asm_out, "  mov %d(%%rbp), %%r11\n", t->op2->offset);
-
-      fprintf(asm_out, "  cmp $0, %%r11\n");
-      fprintf(asm_out, "  setne %%bl\n");
-      fprintf(asm_out, "  and %%bl, %%al\n");
-      fprintf(asm_out, "  movzbq %%al, %%r10\n");
-      fprintf(asm_out, "  mov %%r10, %d(%%rbp)\n", t->result->offset);
+    case TAC_AND:
+      assembly_module_and(asm_out, t);
       break;
-    }
 
-    case TAC_OR: {
-      reset_arg_registers();
-      // booleanize op1 and op2, then or
-      if (t->op1->offset == 0)
-        fprintf(asm_out, "  mov $%d, %%r10\n", t->op1->value);
-      else
-        fprintf(asm_out, "  mov %d(%%rbp), %%r10\n", t->op1->offset);
-
-      fprintf(asm_out, "  cmp $0, %%r10\n");
-      fprintf(asm_out, "  setne %%al\n");
-
-      if (t->op2->offset == 0)
-        fprintf(asm_out, "  mov $%d, %%r11\n", t->op2->value);
-      else
-        fprintf(asm_out, "  mov %d(%%rbp), %%r11\n", t->op2->offset);
-
-      fprintf(asm_out, "  cmp $0, %%r11\n");
-      fprintf(asm_out, "  setne %%bl\n");
-      fprintf(asm_out, "  or %%bl, %%al\n");
-      fprintf(asm_out, "  movzbq %%al, %%r10\n");
-      fprintf(asm_out, "  mov %%r10, %d(%%rbp)\n", t->result->offset);
+    case TAC_OR:
+      assembly_module_or(asm_out, t);
       break;
-    }
 
-    case TAC_NOT: {
-      reset_arg_registers();
-      if (t->op1->offset == 0)
-        fprintf(asm_out, "  mov $%d, %%r10\n", t->op1->value);
-      else
-        fprintf(asm_out, "  mov %d(%%rbp), %%r10\n", t->op1->offset);
-
-      fprintf(asm_out, "  cmp $0, %%r10\n");
-      fprintf(asm_out, "  sete %%al\n");
-      fprintf(asm_out, "  movzbq %%al, %%r10\n");
-      fprintf(asm_out, "  mov %%r10, %d(%%rbp)\n", t->result->offset);
+    case TAC_NOT:
+      assembly_module_not(asm_out, t);
       break;
-    }
 
     // --- Unaries ---
-    case TAC_NEG: {
-      reset_arg_registers();
-      if (t->op1->offset == 0)
-        fprintf(asm_out, "  mov $%d, %%r10\n", t->op1->value);
-      else
-        fprintf(asm_out, "  mov %d(%%rbp), %%r10\n", t->op1->offset);
-
-      fprintf(asm_out, "  neg %%r10\n");
-      fprintf(asm_out, "  mov %%r10, %d(%%rbp)\n", t->result->offset);
+    case TAC_NEG:
+      assembly_module_neg(asm_out, t);
       break;
-    }
 
     // --- Assign ---
-    case TAC_ASSIGN: {
-      reset_arg_registers();
-      if (t->op1->offset == 0)
-        fprintf(asm_out, "  mov $%d, %%r10\n", t->op1->value);
-      else
-        fprintf(asm_out, "  mov %d(%%rbp), %%r10\n", t->op1->offset);
-
-      fprintf(asm_out, "  mov %%r10, %d(%%rbp)\n", t->result->offset);
+    case TAC_ASSIGN:
+      assembly_module_assign(asm_out, t);
       break;
-    }
 
     // --- Flow control ---
     case TAC_LABEL:
-      reset_arg_registers();
-      fprintf(asm_out, "  .globl %s\n", t->result->name);
-      fprintf(asm_out, "%s:\n", t->result->name);
-      // If t->result->offset represents amount to reserve, it keeps.
-      fprintf(asm_out, "  enter $(8 * %d), $0\n", t->result->offset);
+      assembly_module_label(asm_out, t);
       break;
 
     case TAC_LABEL_END:
     case TAC_LABEL_IF:
-      reset_arg_registers();
-      fprintf(asm_out, "%s:\n", t->result->name);
+      assembly_module_label_if(asm_out, t);
       break;
 
     case TAC_GOTO:
-      reset_arg_registers();
-      fprintf(asm_out, "  jmp %s\n", t->result->name);
+      assembly_module_goto(asm_out, t);
       break;
 
     case TAC_IFZ:
-      reset_arg_registers();
-      if (t->op1->offset == 0)
-        fprintf(asm_out, "  mov $%d, %%r10\n", t->op1->value);
-      else
-        fprintf(asm_out, "  mov %d(%%rbp), %%r10\n", t->op1->offset);
-
-      fprintf(asm_out, "  mov $1, %%r11\n");
-      fprintf(asm_out, "  cmp %%r10, %%r11\n");
-      fprintf(asm_out, "  jne %s\n", t->result->name);
+      assembly_module_ifz(asm_out, t);
       break;
 
     // --- Functions ---
-    case TAC_PARAM: {
-      const char *where = get_arg_register();
-      if (where[0] == '%') {
-        // Register --> memory (valid)
-        fprintf(asm_out, "  mov %s, %d(%%rbp)\n", where, t->op1->offset);
-      } else {
-        // Memory --> memory (not allowed) --> go through a temp register
-        fprintf(asm_out, "  mov %s, %%r10\n", where);
-        fprintf(asm_out, "  mov %%r10, %d(%%rbp)\n", t->op1->offset);
-      }
+    case TAC_PARAM:
+      assembly_module_param(asm_out, t);
       break;
-    }
 
-    case TAC_ARG: {
-      // Prepare argument (registers or stack) --> use get_arg_register() once
-      const char *where = get_arg_register();
-      if (t->op1->offset == 0) {
-        // Immediate
-        if (where[0] == '%')
-          fprintf(asm_out, "  mov $%d, %s\n", t->op1->value, where);
-        else
-          fprintf(asm_out, "  movq $%d, %s\n", t->op1->value, where);
-      } else {
-        // Value at local memory/temporal
-        if (where[0] == '%') {
-          fprintf(asm_out, "  mov %d(%%rbp), %s\n", t->op1->offset, where);
-        } else {
-          // Memory --> memory is not allowed; go through a temp register
-          fprintf(asm_out, "  mov %d(%%rbp), %%r10\n", t->op1->offset);
-          fprintf(asm_out, "  movq %%r10, %s\n", where);
-        }
-      }
+    case TAC_ARG:
+      assembly_module_arg(asm_out, t);
       break;
-    }
 
     case TAC_CALL:
-      reset_arg_registers();
-      fprintf(asm_out, "  call %s\n", t->op1->name);
-      // t->op1->offset assumed to be count of pushed args (caller
-      // responsibility)
-      if (t->result)
-        fprintf(asm_out, "  mov %%rax, %d(%%rbp)\n", t->result->offset);
+      assembly_module_call(asm_out, t);
       break;
 
     case TAC_RETURN:
-      reset_arg_registers();
-      if (t->result) {
-        if (t->result->offset == 0)
-          fprintf(asm_out, "  mov $%d, %%rax\n", t->result->value);
-        else
-          fprintf(asm_out, "  mov %d(%%rbp), %%rax\n", t->result->offset);
-      }
-      fprintf(asm_out, "  leave\n");
-      fprintf(asm_out, "  ret\n");
-
+      assembly_module_return(asm_out, t);
       break;
 
     case TAC_EXTERN:
-      fprintf(asm_out, ".extern %s\n", t->op1->name);
+      assembly_module_extern(asm_out, t);
       break;
 
     case TAC_UNKNOWN:
