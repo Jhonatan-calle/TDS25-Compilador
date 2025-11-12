d016555652c7f312a444d949ad5cdd60949b1ac3
Joaquin Tissera <phoenix_css@hotmail.com>
2025-11-12 09:10:17 -0300

    update tests

--- eq_assert.c
+++ eq_assert.c
@@ -4,12 +4,10 @@
 #define COLOR_RED   "\033[0;31m"
 #define COLOR_RESET "\033[0m"
 
-int eq_assert(int expected, int result) {
+void eq_assert(unsigned int expected, unsigned int result) {
   if (expected == result) {
     printf(COLOR_GREEN "Test pass\n" COLOR_RESET);
-    return 1;
   } else {
     printf(COLOR_RED "Test failed\n" COLOR_RESET);
-    return 0;
   }
 }
