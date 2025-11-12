3577b0c1fca55dea85f783dce2bf1217a821d012
Joaquin Tissera <phoenix_css@hotmail.com>
2025-11-12 10:23:29 -0300

    Individual execute

--- us_gen_assembly.sh
+++ us_gen_assembly.sh
@@ -25,11 +25,11 @@ assembly_file="${input_file%.ass}.s"
 # Copy contents from .ass to .s
 cp "$input_file" "$assembly_file"
 
+# printInt
 if [ ! -f "printInt.c" ]; then
     echo "Error: Missing file printInt.c (needed for linking)"
     exit 1
 fi
-
 echo "Compiling extern printInt..."
 gcc -c printInt.c -o printInt.o || {
     echo "Error compiling printInt.c"
@@ -37,10 +37,35 @@ gcc -c printInt.c -o printInt.o || {
     exit 1
 }
 
-echo "Compiling and linking with extern printInt..."
-gcc "$assembly_file" printInt.o -o us_file.out || {
+# getInt
+if [ ! -f "getInt.c" ]; then
+    echo "Error: Missing file getInt.c (needed for linking)"
+    exit 1
+fi
+echo "Compiling extern getInt..."
+gcc -c getInt.c -o getInt.o || {
+    echo "Error compiling getInt.c"
+    exit 1
+}
+
+# eq_assert
+if [ ! -f "eq_assert.c" ]; then
+    echo "Error: Missing file eq_assert.c (needed for linking)"
+    exit 1
+fi
+echo "Compiling extern eq_assert..."
+gcc -c eq_assert.c -o eq_assert.o || {
+    echo "Error compiling eq_assert.c"
+    exit 1
+}
+
+echo "Compiling and linking with extern printInt and getInt and eq_assert..."
+cp last_generated_assembly.ass last_generated_assembly.s
+gcc "$assembly_file" printInt.o getInt.o eq_assert.o -o us_file.out || {
     echo "Compilation failed."
-    rm -f "$assembly_file" printInt.o
+    rm -f printInt.o
+    rm -f getInt.o
+    rm -f eq_assert.o
     exit 1
 }
 
